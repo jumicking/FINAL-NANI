c41402cddce3766b17e87e107fede5f098c956e1
Jumayca Malilong <malilongjumayca@g.cjc.edu.ph>
2025-02-27 21:31:30 +0900

    Add files via upload

---
+++ jums_store/src/jums_store/Jums_store.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jums_store;
+
+/**
+ *
+ * @author MALILONG_CpE121
+ */
+public class Jums_store {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
